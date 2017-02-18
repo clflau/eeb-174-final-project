8ba06dc3176c8a285ca384912eede46cf94f2d86
Clive Lau <clivelflau@gmail.com>
2017-02-18 00:43:40 -0800

    it works!

--- Aurelia_gene_count_finder.py
+++ Aurelia_gene_count_finder.py
@@ -6,20 +6,24 @@ def GeneCountFinder():
 	import re
 	with open(sys.argv[1]) as gene_quarry:
 		quarry_list = gene_quarry.readlines()
-		with open("/home/eeb177-student/Desktop/eeb-177/final-proj/Aurelia_counts_testfile.matrix", "r") as express_matrix: #open testfile to test script
+		formatted_quarry_list = []
+		for elem in quarry_list[1:]:
+			formatted_quarry_list.append(elem.replace("\n", ""))
+		print("Quarrying: " + str(formatted_quarry_list))
+		with open("/home/eeb177-student/Desktop/eeb-177/final-proj/Aurelia_RSEM_10-18-16.TMM.EXPR.100aa.matrix", "r") as express_matrix: 
 			for line in express_matrix:
-				for ii in quarry_list:
-					if re.match(ii, line):
-						data_elem = re.match(re.escape(ii) + r'.*$', line)
+				for ii in formatted_quarry_list:
+					if re.search(ii, line):
+						data_elem = re.search(ii + r'.*$', line)
 						data_list.append(data_elem.group(0))
 	return data_list
 
-GeneCountFinder()
+quarry_result = GeneCountFinder()
 
 ticker = 0
 outfile = open("genes_counts_of_interest.txt", "w")
-while ticker < len(data_list):
-    outfile.write(data_list[ticker] + "\n")
+while ticker < len(quarry_result):
+    outfile.write(quarry_result[ticker] + "\n")
     ticker = ticker + 1
 
 outfile.close()
