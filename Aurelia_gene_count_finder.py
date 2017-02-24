1379175845ef284857383735c88c5e269f650a81
Clive Lau <clivelflau@gmail.com>
2017-02-23 22:06:44 -0800

    changed code to use list comprehension instead of loop

--- Aurelia_gene_count_finder.py
+++ Aurelia_gene_count_finder.py
@@ -1,6 +1,6 @@
 # This python script is a function that takes a txt file containing some gene IDs as argument, and searches for the expression counts of these genes in the transcriptome of Aurelia, and outputs a file of the count data for those genes.
 
-data_list = []
+#data_list = []
 def GeneCountFinder():
 	import sys
 	import re
@@ -9,18 +9,26 @@ def GeneCountFinder():
 		formatted_quarry_list = []
 		for elem in quarry_list[1:]:
 			formatted_quarry_list.append(elem.replace("\n", ""))
-		print("Quarrying: " + str(formatted_quarry_list))
+#		print("Quarrying: " + str(formatted_quarry_list))
 		with open("/home/eeb177-student/Desktop/eeb-177/final-proj/Aurelia_RSEM_10-18-16.TMM.EXPR.100aa.matrix", "r") as express_matrix: 
-			data_list.append(express_matrix.readline())
-			for line in express_matrix:
-				for ii in formatted_quarry_list:
-					if re.search(ii, line):
-						data_elem = re.search(ii + r'.*$', line)
-						data_list.append(data_elem.group(0))
+			first_line = express_matrix.readline().strip()
+#			for line in express_matrix:
+#				for ii in formatted_quarry_list:
+#					if re.search(ii, line):
+#						data_elem = re.search(ii + r'.*$', line)
+#						data_list.append(data_elem.group(0))
+# Try out list comprehension to do the above for loop
+			data_list = [re.search(ii + r'.*$', line).group(0)
+				for line in express_matrix
+				for ii in formatted_quarry_list
+				if re.search(ii, line)
+			]
+			
+			data_list = [first_line] + data_list
 	return data_list
 
 quarry_result = GeneCountFinder()
-
+#print(quarry_result)
 import sys
 ticker = 0
 outfile = open("{}.txt".format(sys.argv[2]), "w")
